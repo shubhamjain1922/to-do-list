61682007b22e4df2288f98db167e2b3fe5fb4eca
shubhamjain1922 <54628090+shubhamjain1922@users.noreply.github.com>
2020-09-11 00:23:56 +0530

    Add files via upload

---
+++ docs/todo.js
@@ -0,0 +1,20 @@
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span",function(event){
+   $(this).parent().fadeOut(500,function(){
+      $(this).remove();
+   });
+   event.stopPropagation();
+
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13)
+    {
+    	var todo = $(this).val();
+    	$(this).val("");
+    	$("ul").append("<li><span>x </span>" + todo + "</li>");
+    }
+});
